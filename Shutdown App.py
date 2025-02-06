ce78673c75276244b163b91226bedbb8dffe6782
Michial Stern <armankhan271609@gmail.com>
2025-02-06 22:18:28 +0530

    Add files via upload

---
+++ Shutdown App.py
@@ -0,0 +1,44 @@
+from tkinter import *
+import os
+
+def restart():
+    os.system("shutdown /r /t 1")
+def restart_time():
+    os.system("shutdown /r /t 20")
+def logout():
+    os.system("shutdown -l")
+def shutdown():
+    os.system("shutdown /s /t 1")
+
+st = Tk()
+st.title("ShutDown App")
+st.geometry("500x500")
+st.config(bg="brown")
+
+# Cretae restart button...
+r_button = Button(st,text="Restart",font=("Time New Roman",21,"bold"), 
+                  relief=RAISED,cursor="plus",command=restart)
+# Set position hight and width...
+r_button.place(x=150,y=60,height=50,width=200)
+
+
+# Cretae restart time button...
+rt_button = Button(st,text="Restart Time",font=("Time New Roman",
+21,"bold"),relief=RAISED,cursor="plus",command=restart_time)
+# Set position hight and width...
+rt_button.place(x=150,y=170,height=50,width=200)
+
+
+# Cretae Log-Out button...
+lg_button = Button(st,text="Log-Out",font=("Time New Roman",21,"bold"), relief=RAISED,cursor="plus",command=logout)
+# Set position hight and width...
+lg_button.place(x=150,y=270,height=50,width=200)
+
+
+# Cretae Shut down  button...
+sd_button = Button(st,text="Shut down",font=("Time New Roman",21,"bold"), relief=RAISED,cursor="plus",command=shutdown)
+# Set position hight and width...
+sd_button.place(x=150,y=370,height=50,width=200)
+
+
+st.mainloop()
